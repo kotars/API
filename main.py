4789ef9c6a4c2a308c71e4879f9730a91a2490d6
Chuma <lokl0k17@mail.ru>
2022-02-01 15:47:46 +0700

    1 задача

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import sys
 import pygame
 import requests
 
-
-def map2(x, y, z=8):
-    map_request = f"https://static-maps.yandex.ru/1.x/?ll={x},{y}&l=sat&z={z}"
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-    map_file = "map.jfif"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
+x = 37.620070
+y = 55.753630
+map_request = f"https://static-maps.yandex.ru/1.x/?ll={x},{y}&l=map&z=13"
+response = requests.get(map_request)
 
 
-x = float(input())
-y = float(input())
-z = 8
-map2(x, y, z)
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
 map_file = "map.jfif"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
@@ -29,23 +25,9 @@ pygame.display.flip()
 clock = pygame.time.Clock()
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYUP:
-            if z < 17:
-                z += 1
-            map2(x, y, z)
-        if event.type == pygame.KEYDOWN:
-            if z > 0:
-                z -= 1
-            map2(x, y, z)
-    map_file = "map.jfif"
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-    clock.tick(50)
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 pygame.quit()
-
 # Удаляем за собой файл с изображением.
 os.remove(map_file)

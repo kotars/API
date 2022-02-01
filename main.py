ae3f3cf2ef7b4b622b1a8a2e70b7b95c70aa0bfb
Chuma <lokl0k17@mail.ru>
2022-02-01 16:03:01 +0700

    задача2

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import sys
 import pygame
 import requests
 
-x = 37.620070
-y = 55.753630
-map_request = f"https://static-maps.yandex.ru/1.x/?ll={x},{y}&l=map&z=13"
-response = requests.get(map_request)
+
+def map2(z):
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&l=map&z={z}"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.jfif"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+z = 8
+map2(z)
 map_file = "map.jfif"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
@@ -25,9 +27,26 @@ pygame.display.flip()
 clock = pygame.time.Clock()
 running = True
 while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-pygame.quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        result = pygame.key.get_pressed()
+        if result[pygame.K_UP]:
+            if z < 17:
+                z += 1
+                map2(z)
+            print(z)
+        if result[pygame.K_DOWN]:
+            if z > 0:
+                z -= 1
+                map2(z)
+            print(z)
+    map_file = "map.jfif"
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    clock.tick(50)
+
+    pygame.quit()
+
 # Удаляем за собой файл с изображением.
 os.remove(map_file)
